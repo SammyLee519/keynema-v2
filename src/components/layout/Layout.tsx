1e58a470a439c432e8bc71b9bc8fb1c39cc77c45
SammyLee <sammul95@gmail.com>
2026-04-21 10:53:35 +0900

    feat: implement footer (#13)
    - Footer 폴더 구조 변경(Footer.tsx -> footer/index.tsx)
    - 로고, 네비게이션, 소셜 아이콘 섹션 구성
    
    Co-authored-by: 샘무리 <sammy@saemmuliui-MacBookPro.local>

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 
 import { HEADER_HEIGHT } from '@/constants'
 
-import Footer from './Footer'
+import Footer from './footer'
 import Header from './header'
 import SideMenu from './sidemenu'
 
